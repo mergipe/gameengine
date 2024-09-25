72b816540a4b8323e9616ed88d8542f6cc4394e3
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2024-09-25 17:49:46 -0300

    implementing Pool class

--- src/scene/ECS.h
+++ src/scene/ECS.h
@@ -7,12 +7,12 @@
 constexpr unsigned int MAX_COMPONENTS{32};
 using Signature = std::bitset<MAX_COMPONENTS>;
 
-struct BaseComponent {
+struct IComponent {
 protected:
     static inline int nextId{};
 };
 
-template <typename T> class Component : public BaseComponent {
+template <typename T> class Component : public IComponent {
     static int getId() {
         const static auto id{nextId++};
         return id;
@@ -44,7 +44,33 @@ public:
     template <typename T> void requireComponent();
 };
 
-class EntityManager {};
+class IPool {
+public:
+    virtual ~IPool() {}
+};
+
+template <typename T> class Pool : public IPool {
+private:
+    std::vector<T> objects{};
+
+public:
+    Pool(int size = 100) { objects.resize(size); }
+    virtual ~Pool() = default;
+    bool isEmpty() const { return objects.empty(); }
+    int getSize() const { return objects.size(); }
+    void resize() { objects.resize(); }
+    void clear() { objects.clear(); }
+    void add(T object) { objects.push_back(object); }
+    void set(int index, T object) { object[index] = object; }
+    T &get(int index) { return static_cast<T &>(objects[index]); }
+    T &operator[](unsigned int index) { return objects[index]; }
+};
+
+class Registry {
+private:
+    int entitiesCount{0};
+    std::vector<IPool *> componentPools{};
+};
 
 template <typename T> void System::requireComponent() {
     const auto componentId{Component<T>::getId()};
