58db39f57e2f8ea714f16e0a3f7a6749898be80a
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2024-09-27 17:46:06 -0300

    improving Logger

--- src/core/Logger.cpp
+++ src/core/Logger.cpp
@@ -1,7 +1,17 @@
 #include <Logger.h>
+#include <memory>
+#include <spdlog/cfg/env.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 void Logger::init() {
-#ifndef NDEBUG
-    spdlog::set_level(spdlog::level::debug);
-#endif
+    const auto console_sink{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()};
+    console_sink->set_level(spdlog::level::trace);
+    const auto file_sink{std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true)};
+    file_sink->set_level(spdlog::level::trace);
+    const std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+    logger = std::make_shared<spdlog::logger>("logger", sinks.begin(), sinks.end());
+    spdlog::register_logger(logger);
+    spdlog::cfg::load_env_levels();
+    Logger::trace("Logger initialized");
 }
--- src/core/Logger.h
+++ src/core/Logger.h
@@ -4,25 +4,46 @@
 #include <spdlog/spdlog.h>
 
 class Logger {
+private:
+    static inline std::shared_ptr<spdlog::logger> logger{};
+
 public:
     static void init();
 
-    template <typename T> static void debug(const T &msg) { spdlog::debug(msg); }
+    template <typename T> static void trace(const T &msg) { logger->trace(msg); }
+
+    template <typename... Args> static void trace(fmt::format_string<Args...> fmt, Args &&...args) {
+        logger->trace(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename T> static void debug(const T &msg) { logger->debug(msg); }
 
     template <typename... Args> static void debug(fmt::format_string<Args...> fmt, Args &&...args) {
-        spdlog::debug(fmt, std::forward<Args>(args)...);
+        logger->debug(fmt, std::forward<Args>(args)...);
     }
 
-    template <typename T> static void info(const T &msg) { spdlog::info(msg); }
+    template <typename T> static void info(const T &msg) { logger->info(msg); }
 
     template <typename... Args> static void info(fmt::format_string<Args...> fmt, Args &&...args) {
-        spdlog::info(fmt, std::forward<Args>(args)...);
+        logger->info(fmt, std::forward<Args>(args)...);
     }
 
-    template <typename T> static void critical(const T &msg) { spdlog::critical(msg); }
+    template <typename T> static void warn(const T &msg) { logger->warn(msg); }
+
+    template <typename... Args> static void warn(fmt::format_string<Args...> fmt, Args &&...args) {
+        logger->warn(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename T> static void error(const T &msg) { logger->error(msg); }
+
+    template <typename... Args> static void error(fmt::format_string<Args...> fmt, Args &&...args) {
+        logger->error(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename T> static void critical(const T &msg) { logger->critical(msg); }
 
     template <typename... Args> static void critical(fmt::format_string<Args...> fmt, Args &&...args) {
-        spdlog::info(fmt, std::forward<Args>(args)...);
+        logger->info(fmt, std::forward<Args>(args)...);
     }
 };
 
